5dc5b91694dfb2e8e35e67dfad8857ff8482e429
Patrick Goulding <patrickgoulding@gmail.com>
2019-05-27 13:34:32 -0600

    removeSurvey method added
    co-authored-by: Aidan McKay <aidanmckay@gmail.com>

--- src/Round.js
+++ src/Round.js
@@ -1,42 +1,23 @@
 import data from "../data";
 import Turn from "./Turn";
 import domUpdates from "./domUpdates";
-// import domUpdates from './domUpdates';
 class Round {
   constructor (currentRound, currentPlayer, num) {
     this.randomNum = num || (Math.floor(Math.random() * (15 - 1 + 1)) + 1);
     this.survey = (this.pullSurveys(this.randomNum));
     this.currentRound = currentRound;
     this.currentPlayer = currentPlayer;
-    this.turn = new Turn(this.survey);
+    this.turn = new Turn(this.survey, currentPlayer);
   }
 
   pullSurveys(randomSurveyID) {
-    // const randomSurveys = []
     let survey = data.surveys.find(survey => survey.id === randomSurveyID);
-    // let survey2 = data.surveys.find(survey => survey.id === randomSurveyID - 1);
-    // let survey3 = data.surveys.find(survey => survey.id === randomSurveyID + 1);
-    // randomSurveys.push(survey1);
-    // let idx = data.surveys.indexOf(survey1)
-    // data.surveys.splice(idx, 1)
-    // console.log(randomSurveys);
-    
     return survey;
   }
 
-
-  // changeRound() {
-  //   if (this.turn.guessed.length === 3) {
-  //     // this.currentRound++
-  //     // this.turn = new Turn(this.survey, this)
-  //     let that = this;
-  //     // setTimeout(() => {
-  //     //   domUpdates.showBoard(that, (that.currentRound - 1), that.currentPlayer)
-  //     // }, 2000);
-  //   }
-  // }
-  changePlayer(player) {
-    this.currentPlayer = player;
+  removeSurvey() {
+    let index = data.surveys.indexOf(this.survey)
+    return data.surveys.splice(index, 1)
   }
 } 
 
